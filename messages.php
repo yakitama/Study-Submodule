546cc4c1b515208ce4c9fd30dcc9592a5f57876b
yakitama <pikaiv@gmail.com>
2018-09-21 11:24:34 +0900

    定型文ファイルを準備した。インクルードして使ってください。

---
+++ messages.php
@@ -0,0 +1,6 @@
+<?php
+// 定型文を用意しているファイル
+$message['good_morning'] = 'おはようございます。';
+$message['good_afternoon'] = 'こんにちは。';
+$message['good_evening'] = 'こんばんは。';
+$message['good_night'] = 'おやすみなさい。';
